506c878a16c1678dfb0c9abc65662989c0d486ca
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-09-13 03:50:15 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -85,9 +85,9 @@ def adding_matz
         }
      }
      
-     programmer_hash[:yukihiro_matsumoto] = {
-       known_for: "Ruby",
-       languages: ["LISP, C"]}
+     programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP, C"]}
+     
+     programmer_hash
 
     
 end
